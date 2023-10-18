4db94916396905cf9a3cbccea3a99080aff03794
Vitor Jenna <vitor3671k@gmail.com>
2023-10-17 23:57:42 -0300

    Commit Quick java

--- Trabalhos Praticos/Trabalho Pratico 02/Java files/QuickJava.java
+++ Trabalhos Praticos/Trabalho Pratico 02/Java files/QuickJava.java
@@ -1,10 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
-//Class Jogadores
+//Class Jogador
 
 class Jogadores {
 
@@ -129,7 +127,7 @@ class Jogadores {
 
     // funções normais
 
-    public String dados() { // print todos os dados do Jogadores
+    public String dados() { // print todos os dados do jogador
         return ("[" +
                 getId() +
                 " ## " +
@@ -151,123 +149,76 @@ class Jogadores {
 }
 
 // MAIN
-public class CountingJava {
+public class QuickJava {
 
-    // contador de comparações
-    static int contador = 0, moves = 0;
-    static long duration;
+    // pesquisa sequencial
+    public static void doQuick(Jogadores jogadores[], int esq, int dir) {
+        int i = esq, j = dir;
+        Jogadores pivo = jogadores[(dir + esq) / 2];
+        Jogadores aux;
 
-    // Criar log
-    public static void criarLog() {
-        String fileName = "/tmp/810862_countingsort.txt";
+        while (i <= j) {
+            while (jogadores[i].getEstadoNascimento().compareTo(pivo.getEstadoNascimento()) < 0) {
+                i++;
 
-        try {
-            File logFile = new File(fileName);
-            FileWriter writer = new FileWriter(logFile);
-            writer.write("810862" + "\t" +contador + "\t" + moves + "\t" +  duration);
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            }
+
+            while (jogadores[j].getEstadoNascimento().compareTo(pivo.getEstadoNascimento()) > 0) {
+                j--;
+
+            }
+
+            if (i <= j) {
+
+                aux = jogadores[i];
+                jogadores[i] = jogadores[j];
+                jogadores[j] = aux;
+                i++;
+                j--;
+
+            }
+
+        }
+
+        if (esq < j) {
+            doQuick(jogadores, esq, j);
+        }
+
+        if (i < dir) {
+            doQuick(jogadores, i, dir);
         }
     }
 
-    public static void alfabetica(int fim, Jogadores jogadores[]) {
+    // funcao para ordenar os jogadores com base na ordem crescente de peso
+    public static void Quick(Jogadores jogadores[], int inicio, int fim) {
 
-        for (int i = 0; i < fim; i++) { // organiza por ordem alfabetica os Jogadoreses com a mesma altura
-            contador++;
+        doQuick(jogadores, inicio, fim - 1);
+
+        for (int i = 0; i < fim; i++) {
             for (int j = i + 1; j < fim; j++) {
-                contador++;
-                if (jogadores[i].getAltura() == jogadores[j].getAltura()) {
-                    contador++;
-                    if (jogadores[i].getNome().compareToIgnoreCase(jogadores[j].getNome()) > 0) {
-                        contador++;
-                        Jogadores aux = jogadores[j];
-                        jogadores[j] = jogadores[i];
-                        jogadores[i] = aux;
-                        moves++;
+
+                if (jogadores[i].getEstadoNascimento().compareTo(jogadores[j].getEstadoNascimento()) == 0) {
+                    if (jogadores[i].getNome().compareTo(jogadores[j].getNome()) > 0) {
+                        Jogadores aux = jogadores[i];
+                        jogadores[i] = jogadores[j];
+                        jogadores[j] = aux;
+
                     }
                 }
             }
+
         }
-    }
-
-    // ordenar os Jogadoreses
-    public static void countSort(int fim, Jogadores jogadores[]) {
-        // tempo de inicio de execução
-        long startTime = System.nanoTime();
-
-        int maior = jogadores[0].getAltura();
-        Jogadores[] jogadoresC = new Jogadores[fim];
-
-        // achar o maior elemento do array
-        for (int i = 0; i < fim; i++) {
-            contador++;
-            if (jogadores[i].getAltura() > maior) {
-                maior = jogadores[i].getAltura();
-            }
-        }
-
-        // Agora vou só deixar na forma alfabetica
-
-        // jarray auxiliar para contar as ocorrencias dos elementos
-
-        int[] aux = new int[maior + 1];
-
-        // inicialuza o array auxiliar
-        for (int i = 0; i <= maior; i++) {
-            contador++;
-            aux[i] = 0;
-        }
-
-        // Exemplo: altura 4 aparece 3 vezes, então aux[4] = 3
-        for (int i = 0; i < fim; i++) {
-            contador++;
-            aux[jogadores[i].getAltura()]++;
-        }
-
-        // Modificar o array auxiliar para que possa ser feito a colocação no JogadoresC
-        // Eu tambem to confuso mas é isso mesmo
-        for (int i = 1; i <= maior; i++) {
-            contador++;
-            aux[i] += aux[i - 1];
-        }
-
-        // AGORA A ORDENAÇÃO COMEÇA
-        // Vamo botar os elementos(a partir da ultima repetição dele) na posição
-        // ordenada dele no JogadoresC
-        // vou pegar a altura do primeiro jogador, depois vou ver em qual posição do
-        // auxiliar ele ta, e vou colocar esse jogador no JogadoresC, COM A POSIÇÃO DO
-        // AUXILIAR
-        for (int i = 0; i < fim; i++) {
-            contador++;
-            jogadoresC[aux[jogadores[i].getAltura()] - 1] = jogadores[i];
-            aux[jogadores[i].getAltura()]--;
-            moves++;
-        }
-
-        // Agora só vou trocar os elementos do Jogadores pelo JogadoresC, para ordenar
-        for (int i = 0; i < fim; i++) {
-            contador++;
-            jogadores[i] = jogadoresC[i];
-            moves++;
-        }
-
-        // Vou só colocar as alturas iguais em ordem alfabetica
-        alfabetica(fim, jogadores);
-
-        long endTime = System.nanoTime();// tempo de fim de execução
-        duration = (endTime - startTime);// tempo de execução
-
     }
 
     public static void main(String[] args) {
+
+        ;// tempo de inicio de execução
 
         Scanner scan = new Scanner(System.in);
         File tabela = new File("/tmp/players.csv");
 
         // variaveis e arrays inicializados
-        Jogadores[] jocker = new Jogadores[3000];
+        Jogadores[] jocker = new Jogadores[500];
         String pedido;
         int numeroJogadores = 0;
 
@@ -275,23 +226,36 @@ public class CountingJava {
             pedido = scan.nextLine();
 
             if (!pedido.equalsIgnoreCase("FIM")) {
-                Jogadores player = new Jogadores(pedido, tabela); // cria um Jogadores novo e leva o pedido para o
+                Jogadores player = new Jogadores(pedido, tabela); // cria um jogadores novo e leva o pedido para o
                                                                   // construtor
-                jocker[numeroJogadores] = player; // ARMAZENA O Jogadores NO ARRAY
+                jocker[numeroJogadores] = player; // ARMAZENA O JOGADOR NO ARRAY
                 numeroJogadores++;
             }
         } while (!pedido.equalsIgnoreCase("FIM"));
         scan.close();
 
-        // Segunda parte - ordenar os Jogadoreses
+        // Segunda parte - ordenar os jogadores
+        Jogadores[] newJogador = new Jogadores[numeroJogadores];
 
-        countSort(numeroJogadores, jocker);
-
-        // Printar os Jogadoreses ordenados
+        
         for (int i = 0; i < numeroJogadores; i++) {
-            System.out.println(jocker[i].dados());
+            newJogador[i] = jocker[i];}
+
+
+        Quick(newJogador, 0, numeroJogadores);
+       
+       
+        int k = 10;
+        Jogadores[] min = new Jogadores[k];
+       
+        for (int i = 0; i < k; i++) {
+            min[i] = newJogador[i];}
+
+        // Printar os jogadores ordenados
+      for (int i = 0; i < min.length; i++) {
+            System.out.println(min[i].dados());
         }
 
-        criarLog();
     }
+    
 }
